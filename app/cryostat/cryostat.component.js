d312bf97184f27433ac96e71d4e5e5798eb6aa33
George Salukvadze <giosal90@gmail.com>
2018-05-18 22:47:50 +0200

    v 0.17 - Changed the logic behind cryostat and inside view DB connection, now there is just one PHP file that receives an element Id as parameter from URL and uses it in SQL query

--- app/cryostat/cryostat.component.js
+++ app/cryostat/cryostat.component.js
@@ -7,25 +7,25 @@ angular.module('cryostat', []).component('cryostat', {
         var self = this;
 
         this.reload = function () {
-            var temp0 = $http.get("php-db-conn/cryostat.conn.php?elemId=" + PT0104);
-            var temp1 = $http.get("php-db-conn/cryostat.conn.PT0105.php");
-            var temp2 = $http.get("php-db-conn/cryostat.conn.PT0100.php");
-            var temp3 = $http.get("php-db-conn/cryostat.conn.PT0101.php");
-            var temp4 = $http.get("php-db-conn/cryostat.conn.TT0123.php");
-            var temp5 = $http.get("php-db-conn/cryostat.conn.SPAREAI.php");
-            var temp6 = $http.get("php-db-conn/cryostat.conn.MHT0100.php");
-            var temp7 = $http.get("php-db-conn/cryostat.conn.TT0100.php");
-            var temp8 = $http.get("php-db-conn/cryostat.conn.PT0106.php");
-            var temp9 = $http.get("php-db-conn/cryostat.conn.PT0102.php");
-            var temp10 = $http.get("php-db-conn/cryostat.conn.PT0103.php");
-            var temp11 = $http.get("php-db-conn/cryostat.conn.TT0101.php");
-            var temp12 = $http.get("php-db-conn/cryostat.conn.TT0102.php");
-            var temp13 = $http.get("php-db-conn/cryostat.conn.TT0103.php");
-            var temp14 = $http.get("php-db-conn/cryostat.conn.TT0107.php");
-            var temp15 = $http.get("php-db-conn/cryostat.conn.TT0113.php");
-            var temp16 = $http.get("php-db-conn/cryostat.conn.TT0115.php");
-            var temp17 = $http.get("php-db-conn/cryostat.conn.TT0119.php");
-            var temp18 = $http.get("php-db-conn/cryostat.conn.TT0120.php");
+            var temp0 = $http.get("php-db-conn/cryostat.conn.php?elemId=PT0104");
+            var temp1 = $http.get("php-db-conn/cryostat.conn.php?elemId=PT0105");
+            var temp2 = $http.get("php-db-conn/cryostat.conn.php?elemId=PT0100");
+            var temp3 = $http.get("php-db-conn/cryostat.conn.php?elemId=PT0101");
+            var temp4 = $http.get("php-db-conn/cryostat.conn.php?elemId=TT0123");
+            var temp5 = $http.get("php-db-conn/cryostat.conn.php?elemId=SPAREAI");
+            var temp6 = $http.get("php-db-conn/cryostat.conn.php?elemId=MHT0100");
+            var temp7 = $http.get("php-db-conn/cryostat.conn.php?elemId=TT0100");
+            var temp8 = $http.get("php-db-conn/cryostat.conn.php?elemId=PT0106");
+            var temp9 = $http.get("php-db-conn/cryostat.conn.php?elemId=PT0102");
+            var temp10 = $http.get("php-db-conn/cryostat.conn.php?elemId=PT0103");
+            var temp11 = $http.get("php-db-conn/cryostat.conn.php?elemId=TT0101");
+            var temp12 = $http.get("php-db-conn/cryostat.conn.php?elemId=TT0102");
+            var temp13 = $http.get("php-db-conn/cryostat.conn.php?elemId=TT0103");
+            var temp14 = $http.get("php-db-conn/cryostat.conn.php?elemId=TT0107");
+            var temp15 = $http.get("php-db-conn/cryostat.conn.php?elemId=TT0113");
+            var temp16 = $http.get("php-db-conn/cryostat.conn.php?elemId=TT0115");
+            var temp17 = $http.get("php-db-conn/cryostat.conn.php?elemId=TT0119");
+            var temp18 = $http.get("php-db-conn/cryostat.conn.php?elemId=TT0120");
             var temp19 = $http.get('php-db-conn/cryostat.conn.timestamp.php');
             $q.all([temp0, temp1, temp2, temp3, temp4, temp5, temp6, temp7, temp8, temp9, temp10, temp11, temp12, temp13, temp14, temp15, temp16, temp17, temp18, temp19]).then(function (resultArray) {
                 self.PT0104 = resultArray[0].data.records;
