6412450d5b0cf1b2c8558dd46cf533e3bc19b6e4
George Salukvadze <giosal90@gmail.com>
2018-05-24 15:38:22 +0200

    v 0.19.1 - Updated fonts in CSS to include websafe fonts - Updated Membranes page to be readable - Changed logic behind Histogram to prepare for buttons

--- app/histogram/histogram.component.js
+++ app/histogram/histogram.component.js
@@ -12,78 +12,84 @@ angular.module('histogram', []).component('histogram', {
             }
             this.pageTitle = this.elemId;
             this.natalie = 1;
-            this.TT0101 = "";
             let self = this;
             let labels = [];
             let values = [];
-            $http.get("php-db-conn/histogram.conn.php?elemId=" + this.elemId + "&days=" + this.days).then(function (response) {
-                let title = self.elemId;
-                self.elemId = response.data.records;
-                for (let i in self.elemId) {
-                    labels.push(moment((self.elemId[i].TimeStamp), "DD-MMM-YYYY hh.mm.ss.SSSSSSSSS A"));
-                    values.push(self.elemId[i].ExactValue);
-                }
-                let chartdata = {
-                    labels: labels,
-                    datasets: [
-                        {
-                            label: title,
-                            tension: 0,
-                            pointRadius: 1,
-                            backgroundColor: 'rgba(200, 200, 200, 0.001)',
-                            borderWidth: 1,
-                            borderColor: 'rgba(200, 200, 200, 0.75)',
-                            hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
-                            hoverBorderColor: 'rgba(200, 200, 200, 1)',
-                            data: values
+
+            this.dayChanger = function (fundays) {
+                console.log(fundays);
+                let url = "#!/histogram/" + this.elemId + "/" + fundays;
+                $http.get(url).then(function() {
+                    $http.get("php-db-conn/histogram.conn.php?elemId=" + self.elemId + "&days=" + fundays).then(function (response) {
+                        let title = self.elemId;
+                        let respdata = response.data.records;
+                        for (let i in respdata) {
+                            labels.push(moment((respdata[i].TimeStamp), "DD-MMM-YYYY hh.mm.ss.SSSSSSSSS A"));
+                            values.push(respdata[i].ExactValue);
                         }
-                    ]
-                };
-                let ctx = $('#hist');
+                        let chartdata = {
+                            labels: labels,
+                            datasets: [
+                                {
+                                    label: title,
+                                    tension: 0,
+                                    pointRadius: 1,
+                                    backgroundColor: 'rgba(200, 200, 200, 0.001)',
+                                    borderWidth: 1,
+                                    borderColor: 'rgba(200, 200, 200, 0.75)',
+                                    hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
+                                    hoverBorderColor: 'rgba(200, 200, 200, 1)',
+                                    data: values
+                                }
+                            ]
+                        };
+                        let ctx = $('#hist');
 
-                let barGraph = new Chart(ctx, {
-                    type: 'line',
-                    data: chartdata,
-                    options: {
-                        legend: {
-                            labels: {
-                                fontColor: "white",
-                                fontSize: 18
-                            }
-
-                        },
-                        scales: {
-                            xAxes: [{
-                                type: "time",
-                                time: {
-                                    tooltipFormat: "DD/MM/YYYY HH:mm:ss",
-                                    displayFormats: {
-                                        millisecond: "HH:mm:ss.SSS",
-                                        second: "HH:mm:ss",
-                                        minute: "HH:mm",
-                                        hour: "DD/MM HH:mm",
-                                        day: "DD/MM/YYYY",
-                                        week: "DD/MM",
-                                        month: "MM YYYY",
-                                        year: "YYYY"
+                        let barGraph = new Chart(ctx, {
+                            type: 'line',
+                            data: chartdata,
+                            options: {
+                                legend: {
+                                    labels: {
+                                        fontColor: "white",
+                                        fontSize: 18
                                     }
+
                                 },
-                                ticks: {
+                                scales: {
+                                    xAxes: [{
+                                        type: "time",
+                                        time: {
+                                            tooltipFormat: "DD/MM/YYYY HH:mm:ss",
+                                            displayFormats: {
+                                                millisecond: "HH:mm:ss.SSS",
+                                                second: "HH:mm:ss",
+                                                minute: "HH:mm",
+                                                hour: "DD/MM HH:mm",
+                                                day: "DD/MM/YYYY",
+                                                week: "DD/MM",
+                                                month: "MM YYYY",
+                                                year: "YYYY"
+                                            }
+                                        },
+                                        ticks: {
 
-                                    fontColor: "white"
+                                            fontColor: "white"
+                                        }
+                                    }],
+                                    yAxes: [{
+                                        ticks: {
+                                            fontColor: "white"
+                                        }
+                                    }]
                                 }
-                            }],
-                            yAxes: [{
-                                ticks: {
-                                    fontColor: "white"
-                                }
-
-                            }]
-                        }
-                    }
+                            }
+                        });
+                    });
                 });
-                console.log(labels);
-            });
+            };
+            this.dayChanger(this.days);
+
         }
     ]
 });
