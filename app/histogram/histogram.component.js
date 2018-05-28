bb956391bd07c3759ea14ffa0d4f2865c4c07210
George Salukvadze <giosal90@gmail.com>
2018-05-28 11:13:03 +0200

    v 0.19.1 - Updated fonts in CSS to include websafe fonts - Updated Membranes page to be readable - Changed logic behind Histogram to prepare for buttons

--- app/histogram/histogram.component.js
+++ app/histogram/histogram.component.js
@@ -23,68 +23,92 @@ angular.module('histogram', []).component('histogram', {
                     $http.get("php-db-conn/histogram.conn.php?elemId=" + self.elemId + "&days=" + fundays).then(function (response) {
                         let title = self.elemId;
                         let respdata = response.data.records;
-                        for (let i in respdata) {
-                            labels.push(moment((respdata[i].TimeStamp), "DD-MMM-YYYY hh.mm.ss.SSSSSSSSS A"));
-                            values.push(respdata[i].ExactValue);
-                        }
-                        let chartdata = {
-                            labels: labels,
-                            datasets: [
-                                {
-                                    label: title,
-                                    tension: 0,
-                                    pointRadius: 1,
-                                    backgroundColor: 'rgba(200, 200, 200, 0.001)',
-                                    borderWidth: 1,
-                                    borderColor: 'rgba(200, 200, 200, 0.75)',
-                                    hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
-                                    hoverBorderColor: 'rgba(200, 200, 200, 1)',
-                                    data: values
-                                }
-                            ]
-                        };
-                        let ctx = $('#hist');
-
-                        let barGraph = new Chart(ctx, {
-                            type: 'line',
-                            data: chartdata,
-                            options: {
-                                legend: {
-                                    labels: {
-                                        fontColor: "white",
-                                        fontSize: 18
-                                    }
-
-                                },
-                                scales: {
-                                    xAxes: [{
-                                        type: "time",
-                                        time: {
-                                            tooltipFormat: "DD/MM/YYYY HH:mm:ss",
-                                            displayFormats: {
-                                                millisecond: "HH:mm:ss.SSS",
-                                                second: "HH:mm:ss",
-                                                minute: "HH:mm",
-                                                hour: "DD/MM HH:mm",
-                                                day: "DD/MM/YYYY",
-                                                week: "DD/MM",
-                                                month: "MM YYYY",
-                                                year: "YYYY"
-                                            }
-                                        },
-                                        ticks: {
-
-                                            fontColor: "white"
-                                        }
-                                    }],
-                                    yAxes: [{
-                                        ticks: {
-                                            fontColor: "white"
-                                        }
-                                    }]
-                                }
+                        if (respdata == '{"records":]}') {
+                            console.log("JSON error");
+                            this.dayChanger(7);
+                        } else {
+                            for (let i in respdata) {
+                                labels.push(moment((respdata[i].TimeStamp), "DD-MMM-YYYY hh.mm.ss.SSSSSSSSS A"));
+                                values.push(respdata[i].ExactValue);
                             }
-                        });
+                            let chartdata = {
+                                labels: labels,
+                                datasets: [
+                                    {
+                                        label: title,
+                                        tension: 0,
+                                        pointRadius: 1,
+                                        backgroundColor: 'rgba(200, 0, 0, 0.001)',
+                                        borderWidth: 1,
+                                        borderColor: 'rgba(200, 0, 0, 0.75)',
+                                        hoverBackgroundColor: 'rgba(200, 0, 0, 1)',
+                                        hoverBorderColor: 'rgba(200, 0, 0, 1)',
+                                        data: values
+                                    }
+                                ]
+                            };
+                            let ctx = $('#hist');
+
+                            Chart.pluginService.register({
+                                beforeDraw: function (chart, easing) {
+                                    if (chart.config.options.chartArea && chart.config.options.chartArea.backgroundColor) {
+                                        var helpers = Chart.helpers;
+                                        var ctx = chart.chart.ctx;
+                                        var chartArea = chart.chartArea;
+
+                                        ctx.save();
+                                        ctx.fillStyle = chart.config.options.chartArea.backgroundColor;
+                                        ctx.fillRect(chartArea.left, chartArea.top, chartArea.right - chartArea.left, chartArea.bottom - chartArea.top);
+                                        ctx.restore();
+                                    }
+                                }
+                            });
+
+                            let barGraph = new Chart(ctx, {
+                                type: 'line',
+                                data: chartdata,
+                                options: {
+                                    chartArea: {
+                                        backgroundColor: 'rgba(255, 255, 255, 1)'
+                                    },
+                                    legend: {
+                                        labels: {
+                                            fontColor: "white",
+                                            fontSize: 18
+                                        }
+
+                                    },
+                                    scales: {
+                                        xAxes: [{
+                                            type: "time",
+                                            time: {
+                                                tooltipFormat: "DD/MM/YYYY HH:mm:ss",
+                                                displayFormats: {
+                                                    millisecond: "HH:mm:ss.SSS",
+                                                    second: "HH:mm:ss",
+                                                    minute: "HH:mm",
+                                                    hour: "DD/MM HH:mm",
+                                                    day: "DD/MM/YYYY",
+                                                    week: "DD/MM",
+                                                    month: "MM YYYY",
+                                                    year: "YYYY"
+                                                }
+                                            },
+                                            ticks: {
+
+                                                fontColor: "white"
+                                            }
+                                        }],
+                                        yAxes: [{
+                                            ticks: {
+                                                fontColor: "white"
+                                            }
+                                        }]
+                                    }
+                                }
+
+                            });
+                        }
                     });
                 });
             };
--- app/php-db-conn/histogram.conn.php
+++ app/php-db-conn/histogram.conn.php
@@ -30,6 +30,9 @@ while($rs = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
     $outp .= '"ExactValue":"'. $rs["VALUE_NUMBER"]     . '"},';
 }
     $outp ='{"records":['.$outp.']}';
+    if ($outp == '{"records":]}') {
+        $outp = '{"records": []}';
+    }
     $outp = substr($outp, 0, -3) .substr($outp, -2, 3);
 oci_close($conn);
 
