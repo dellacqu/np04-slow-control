0aa28b25954732296bcf721345cbe2d5946e8e92
George Salukvadze <giosal90@gmail.com>
2018-06-07 12:24:59 +0200

    v 0.25.1 - Added live stream from the detector - Small fixes

--- app/camera/camera.component.js
+++ app/camera/camera.component.js
@@ -1,34 +1,15 @@
 'use strict';
 angular.module('camera', []).component('camera', {
     templateUrl: 'camera/camera.template.html',
-    controller: ['$routeParams', '$http', '$window', '$sce', '$interval',
-        function cameraController($routeParams, $http, $window, $sce, $interval) {
-            this.cameraId = $routeParams.cameraId;
-            this.pageTitle = "camera " + this.cameraId;
+    controller: ['$http', '$window', '$interval',
+        function cameraController($http, $window, $interval) {
+            this.pageTitle = "Live stream";
             this.natalie = 1;
             this.TT0101 = "";
             let self = this;
 
-            this.config = {
-                sources: [
-                    {src: $sce.trustAsResourceUrl("http://protodune-dp01.cern.ch/video1.mjpeg"), type: "video/mppeg"}
-                ],
-                tracks: [
-                    {
-                        src: "http://www.videogular.com/assets/subs/pale-blue-dot.vtt",
-                        kind: "subtitles",
-                        srclang: "en",
-                        label: "English",
-                        default: ""
-                    }
-                ],
-                theme: "css/videogular.css",
-                plugins: {
-                    poster: "http://www.videogular.com/assets/images/videogular.png"
-                }
-            };
 
-            this.reload();
+
             $interval(this.reload, 600000);
         }]
 });
--- app/camera/camera.module.js
+++ app/camera/camera.module.js
@@ -1,9 +1,5 @@
 'use strict';
 angular.module('camera', [
     'ngRoute',
-    "ngSanitize",
-    "com.2fdevs.videogular",
-    "com.2fdevs.videogular.plugins.controls",
-    "com.2fdevs.videogular.plugins.overlayplay",
-    "com.2fdevs.videogular.plugins.poster"
+    "ngSanitize"
 ]);
